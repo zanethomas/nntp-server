9e19cd29677d21ac4a4bc718d818cfc70df732af
Zane Thomas <zane.thomas@gmail.com>
2019-02-03 20:38:29 -0800

    removed logging

--- index.js
+++ index.js
@@ -2,16 +2,12 @@
 //
 'use strict';
 
-const log = require('electron-log');
-
 const glob    = require('glob');
 const net     = require('net');
 const path    = require('path');
 const tls     = require('tls');
 const url     = require('url');
 const Session = require('./lib/session');
-
-log.error('in nntp-server');
 
 const commands = {};
 
